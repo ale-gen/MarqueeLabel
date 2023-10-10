fa3397d0152b1fa36342cc69629d7131c0a6f2e7
Aleksandra Generowicz <62150130+ale-gen@users.noreply.github.com>
2023-10-10 08:47:16 +0200

    Define multiple SPM products for different linking types

--- Package.swift
+++ Package.swift
@@ -13,7 +13,14 @@ let package = Package(
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "MarqueeLabel",
+            targets: ["MarqueeLabel"]),
+        .library(
+            name: "MarqueeLabel-Dynamic",
             type: .dynamic,
+            targets: ["MarqueeLabel"]),
+        .library(
+            name: "MarqueeLabel-Static",
+            type: .static,
             targets: ["MarqueeLabel"]),
     ],
     targets: [
